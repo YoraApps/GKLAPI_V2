4f733b416193471954725f5d24e9beedcd7c6435
vijay jasti <vijay.jasti2010@gmail.com>
2018-09-25 16:30:35 +0530

    ProgramBranchAssociationMaster

---
+++ API/Controllers/ProgramBranchAssociationController.cs
@@ -0,0 +1,34 @@
+﻿using BusinessService.IService;
+using BusinessService.Service;
+using DTO.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class ProgramBranchAssociationController : ApiController
+    {
+        private IProgramBranchAssociationService programBranchAssociationService = new ProgramBranchAssociationService();
+
+        [HttpGet]
+        public IHttpActionResult GetProgramByBranch(int BranchId)
+        {
+            return Ok(new { results = programBranchAssociationService.GetProgramByBranch(BranchId) });
+        }
+
+        public IHttpActionResult GetProgramBranchNotMapped(int BranchId)
+        {
+            return Ok(new { results = programBranchAssociationService.GetProgramBranchNotMapped(BranchId) });
+        }
+
+        [HttpPost]
+        public IHttpActionResult UpdateProgramBranchAssociation(ProgramBranchAssociation programBranchAssociation)
+        {
+            return Ok(new { results = programBranchAssociationService.UpdateProgramBranchAssociation(programBranchAssociation) });
+        }
+    }
+}
---
+++ DataAccess/DataSource/ProgramBranchAssociationDS.cs
@@ -0,0 +1,74 @@
+﻿using DTO.Entity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DataSource
+{
+    public class ProgramBranchAssociationDS
+    {
+        public static List<ProgramBranchAssociation> GetProgramByBranch(int BranchId)
+        {
+            AdoHelper objHelper = new AdoHelper(ConfigurationManager.ConnectionStrings["con"].ToString());
+            DataSet ds = new DataSet();
+            ds = objHelper.ExecDataSetProc("Gkl_USP_GetProgramByBranch");
+
+            List<ProgramBranchAssociation> objlm = null;
+            objlm = ds.Tables[0].AsEnumerable()
+            .Select(row => new ProgramBranchAssociation
+            {
+                ProgramId = row.Field<int>("ProgramId"),
+                BranchId = row.Field<int>("BranchId"),
+                BranchCode = Common.ConvertFromDBVal<string>(row["BranchCode"]),
+                BranchName = Common.ConvertFromDBVal<string>(row["BranchName"]),
+                Active = row.Field<bool>("Active")
+
+            }).ToList();
+
+            return objlm;
+        }
+
+        public static string UpdateProgramBranchAssociation(ProgramBranchAssociation programBranchAssociation)
+        {
+            var Programs = programBranchAssociation.ProgramIds.TrimEnd(',');
+
+            AdoHelper objHelper = new AdoHelper(ConfigurationManager.ConnectionStrings["con"].ToString());
+
+            SqlParameter[] sqlParameter = {
+                            new SqlParameter("@SetAction", programBranchAssociation.SetAction.ToUpper()),
+                            new SqlParameter("@ProgramIds", Programs),
+                            new SqlParameter("@BranchId", programBranchAssociation.BranchId)
+            };
+
+            Object obj = objHelper.ExecScalarProc("Gkl_USP_UpdateProgramBranchAssociation", sqlParameter);
+            string status = "updated";
+            objHelper.Dispose();
+            return status;
+        }
+
+        public static List<ProgramBranchAssociation> GetProgramBranchNotMapped(int BranchId)
+        {
+            AdoHelper objHelper = new AdoHelper(ConfigurationManager.ConnectionStrings["con"].ToString());
+            DataSet ds = new DataSet();
+
+            ds = objHelper.ExecDataSetProc("Gkl_USP_GetProgramBranchNotMapped");
+
+            List<ProgramBranchAssociation> objlm = null;
+            objlm = ds.Tables[0].AsEnumerable()
+            .Select(row => new ProgramBranchAssociation
+            {
+                BranchId = row.Field<int>("BranchId"),
+                BranchCode = Common.ConvertFromDBVal<string>(row["BranchCode"]),
+                BranchName = Common.ConvertFromDBVal<string>(row["BranchName"]),
+
+            }).ToList();
+
+            return objlm;
+        }
+    }
+}
