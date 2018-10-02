0230a45c64ed74b3d10e599c5cafbd278a0d2ac0
Yogesh Rajput <yogeshrajput335@gmail.com>
2018-10-02 21:11:41 +0530

    Added Admission-Criteria

---
+++ API/Controllers/AdmissionCriteriaController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessService.IService;
+using BusinessService.Service;
+using DTO.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class AdmissionCriteriaController : ApiController
+    {
+        private IAdmissionCriteriaService admissionCriteriaService = new AdmissionCriteriaService();
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok(new { results = admissionCriteriaService.GetAdmissionCriteria() });
+        }
+
+        [HttpPost]
+        public IHttpActionResult UpdateAdmissionCriteria (AdmissionCriteria admissionCriteria)
+        {
+            return Ok(new { results = admissionCriteriaService.UpdateAdmissionCriteria(admissionCriteria) });
+        }
+    }
+}
+
---
+++ DTO/Entity/AdmissionCriteria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.Entity
+{
+    public class AdmissionCriteria
+    {
+        public string SetAction { get; set; }
+        public int CriteriaId { get; set; }
+        public string CriteriaDescription { get; set; }
+        public bool Active { get; set; }
+    }
+}
